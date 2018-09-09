1409ec2e40a6496a6dc33a54c8fad8cf7c4d5dbc
Jesse Pavel <jpavel@alum.mit.edu>
2018-09-09 17:11:13 -0400

    Building options dialog

--- InputReplace/src/com/illcode/jedit/inputreplace/OptionPanel.java
+++ InputReplace/src/com/illcode/jedit/inputreplace/OptionPanel.java
@@ -6,6 +6,7 @@ package com.illcode.jedit.inputreplace;
 
 import java.awt.*;
 import javax.swing.*;
+import com.jgoodies.forms.factories.*;
 
 /**
  * @author Jesse Pavel
@@ -17,16 +18,26 @@ public class OptionPanel extends JPanel {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        label1 = new JLabel();
 
         //======== this ========
+        setBorder(Borders.DLU7);
         setLayout(new GridBagLayout());
         ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 0};
         ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0};
         ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
         ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+
+        //---- label1 ----
+        label1.setText("InputReplace Options");
+        label1.setFont(new Font("Dialog", Font.ITALIC, 14));
+        add(label1, new GridBagConstraints(0, 0, 2, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 5, 0), 0, 0));
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    private JLabel label1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
