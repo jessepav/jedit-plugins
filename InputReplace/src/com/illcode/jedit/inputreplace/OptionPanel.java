bc3366e1ffd3eebffb8d9198a1634234c5c184d9
Jesse Pavel <jpavel@alum.mit.edu>
2018-09-09 17:06:30 -0400

    Starting InputReplace plugin

---
+++ InputReplace/src/com/illcode/jedit/inputreplace/OptionPanel.java
@@ -0,0 +1,32 @@
+/*
+ * Created by JFormDesigner on Sun Sep 09 17:05:03 EDT 2018
+ */
+
+package com.illcode.jedit.inputreplace;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author Jesse Pavel
+ */
+public class OptionPanel extends JPanel {
+    public OptionPanel() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        setLayout(new GridBagLayout());
+        ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 0};
+        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
