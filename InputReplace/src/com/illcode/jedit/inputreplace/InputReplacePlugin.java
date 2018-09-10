42b3af42f2d4e9faf96f12db27234f63855da009
Jesse Pavel <jpavel@alum.mit.edu>
2018-09-10 03:17:43 -0400

    Get the plugin infrastructure up

---
+++ InputReplace/src/com/illcode/jedit/inputreplace/InputReplacePlugin.java
@@ -0,0 +1,9 @@
+package com.illcode.jedit.inputreplace;
+
+import org.gjt.sp.jedit.EditPlugin;
+
+public class InputReplacePlugin extends EditPlugin
+{
+    public static final String NAME = "inputreplace";
+    public static final String OPTION_PREFIX = "options.inputreplace.";
+}
