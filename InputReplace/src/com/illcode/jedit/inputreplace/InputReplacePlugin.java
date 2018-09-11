4518d50da4bbc6f6e799083025a590ad554d3f5e
Jesse Pavel <jpavel@alum.mit.edu>
2018-09-11 18:17:39 -0400

    Functionality building up

--- InputReplace/src/com/illcode/jedit/inputreplace/InputReplacePlugin.java
+++ InputReplace/src/com/illcode/jedit/inputreplace/InputReplacePlugin.java
@@ -1,26 +1,57 @@
 package com.illcode.jedit.inputreplace;
 
 import org.gjt.sp.jedit.EditPlugin;
+import org.gjt.sp.jedit.bsh.Interpreter;
+import org.gjt.sp.jedit.jEdit;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public final class InputReplacePlugin extends EditPlugin
 {
     public static final String NAME = "inputreplace";
+
+    private static Interpreter intr;
+    private static Path functionsPath, tablePath;
 
     public static void inputReplace() {
 
     }
 
     public static void reloadResources() {
-
+        intr = new Interpreter();
     }
 
     public static void editTable() {
-
+        jEdit.openFile(jEdit.getActiveView(), tablePath.toString());
     }
 
     public static void editUserFunctions() {
-
+        jEdit.openFile(jEdit.getActiveView(), functionsPath.toString());
     }
 
+    public void start() {
+        ensureResourcesPresent();
+        reloadResources();
+    }
 
+    private void ensureResourcesPresent() {
+        Path pluginHome = getPluginHome().toPath();
+        functionsPath = pluginHome.resolve("input-replace-functions.bsh");
+        tablePath = pluginHome.resolve("input-replace-table.txt");
+        try {
+            if (!Files.exists(functionsPath)) {
+                String defaultFunctions = Utils.getStringResource("/resources/input-replace-functions-default.bsh");
+                Files.write(functionsPath, defaultFunctions.getBytes(StandardCharsets.UTF_8));
+            }
+            if (!Files.exists(tablePath)) {
+                String defaultTable = Utils.getStringResource("/resources/input-replace-table-default.txt");
+                Files.write(tablePath, defaultTable.getBytes(StandardCharsets.UTF_8));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- InputReplace/src/com/illcode/jedit/inputreplace/OptionPanel.java
+++ InputReplace/src/com/illcode/jedit/inputreplace/OptionPanel.java
@@ -19,14 +19,16 @@ public class OptionPanel extends JPanel {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         label1 = new JLabel();
+        label2 = new JLabel();
+        lengthSpinner = new JSpinner();
 
         //======== this ========
         setBorder(new EmptyBorder(5, 5, 5, 5));
         setLayout(new GridBagLayout());
         ((GridBagLayout)getLayout()).columnWidths = new int[] {0, 0, 0};
-        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0, 0};
+        ((GridBagLayout)getLayout()).rowHeights = new int[] {0, 0, 0};
         ((GridBagLayout)getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
-        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+        ((GridBagLayout)getLayout()).rowWeights = new double[] {0.0, 0.0, 1.0E-4};
 
         //---- label1 ----
         label1.setText("InputReplace Options");
@@ -34,10 +36,24 @@ public class OptionPanel extends JPanel {
         add(label1, new GridBagConstraints(0, 0, 2, 1, 0.0, 0.0,
             GridBagConstraints.CENTER, GridBagConstraints.BOTH,
             new Insets(0, 0, 5, 0), 0, 0));
+
+        //---- label2 ----
+        label2.setText("Maximum Replace Length:");
+        add(label2, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 0, 5), 0, 0));
+
+        //---- lengthSpinner ----
+        lengthSpinner.setModel(new SpinnerNumberModel(3, 2, 12, 1));
+        add(lengthSpinner, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+            new Insets(0, 0, 0, 0), 0, 0));
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    private JLabel label1;
+    JLabel label1;
+    JLabel label2;
+    JSpinner lengthSpinner;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
