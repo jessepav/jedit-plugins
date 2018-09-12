e683454a2cf3dc077e4547386fc12a07aba96342
Jesse Pavel <jpavel@alum.mit.edu>
2018-09-12 01:02:26 -0400

    The plugin works!

--- InputReplace/src/com/illcode/jedit/inputreplace/InputReplaceOptionPane.java
+++ InputReplace/src/com/illcode/jedit/inputreplace/InputReplaceOptionPane.java
@@ -1,21 +1,25 @@
 package com.illcode.jedit.inputreplace;
 
 import org.gjt.sp.jedit.AbstractOptionPane;
+import org.gjt.sp.jedit.jEdit;
 
 public class InputReplaceOptionPane extends AbstractOptionPane
 {
     private OptionPanel optionPanel;
 
-    public InputReplaceOptionPane(String internalName) {
-        super(InputReplacePlugin.NAME);
+    public InputReplaceOptionPane() {
+        super("inputreplace");
     }
 
     protected void _init() {
         optionPanel = new OptionPanel();
+        InputReplacePlugin.maxLength = jEdit.getIntegerProperty("inputreplace.max-length", InputReplacePlugin.maxLength);
+        optionPanel.lengthSpinner.setValue(Integer.valueOf(InputReplacePlugin.maxLength));
         addComponent(optionPanel);
     }
 
     protected void _save() {
         InputReplacePlugin.maxLength = (Integer) optionPanel.lengthSpinner.getValue();
+        jEdit.setIntegerProperty("inputreplace.max-length", InputReplacePlugin.maxLength);
     }
 }
