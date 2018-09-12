2b9daa3dfefbdc5cbfa7de87ff0e5e70b9742c7c
Jesse Pavel <jpavel@alum.mit.edu>
2018-09-12 17:33:47 -0400

    Update documentation

--- InputReplace/doc/format-manual-html.bat
+++ InputReplace/doc/format-manual-html.bat
@@ -1,2 +1,2 @@
-FormatAtlantisHTML.bat -w 800 -f 3,2,18 -l 2 -i 10 InputReplace.htm InputReplace.html
+FormatAtlantisHTML.bat -w 850 -f 4,3,20 -l 2 -i 10 InputReplace.htm InputReplace.html
 copy /Y InputReplace.html ..\..\docs\InputReplace.html
