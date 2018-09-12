503a1352711ff9f41dab7a5d11fe72fca62565f8
Jesse Pavel <jpavel@alum.mit.edu>
2018-09-12 17:38:55 -0400

    GitHub page wasn't being updated

--- InputReplace/doc/format-manual-html.bat
+++ InputReplace/doc/format-manual-html.bat
@@ -1,2 +1,2 @@
-FormatAtlantisHTML.bat -w 850 -f 4,3,20 -l 2 -i 10 InputReplace.htm InputReplace.html
+call FormatAtlantisHTML.bat -w 850 -f 4,3,20 -l 2 -i 10 InputReplace.htm InputReplace.html
 copy /Y InputReplace.html ..\..\docs\InputReplace.html
